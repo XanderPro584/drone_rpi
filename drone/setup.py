dbd9d1641e5cf77923c2c9de701763494b7cd3fc
XanderPro584 <agrawal.alexander@gmail.com>
2025-02-05 15:33:28 -0600

    changed arm_drone_client file name to arm_drone

--- drone/setup.py
+++ drone/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'drone_arm_client = drone.arm_drone:main',
+            'arm_drone = drone.arm_drone:main',
         ],
     },
 )
